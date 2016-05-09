52eda22f3861e37d812f597706f4fcc7aa1ed9ad
Nichy Han <vincentvictor@windowslive.com>
2016-05-09 20:03:51 +0700

    init nichy

--- src/js/route/route.js
+++ src/js/route/route.js
@@ -2,16 +2,28 @@ angular.module('todoApp')
 .config(function($stateProvider, $urlRouterProvider) {
   //
   // For any unmatched url, redirect to /home
-  $urlRouterProvider.otherwise("/home");
+  //$urlRouterProvider.otherwise("/login");
   //
   // Now set up the states
   $stateProvider
-    .state('home', {
-      url: "/home",
-      templateUrl: "src/view/home.tmpl"
+    .state('login', {
+      url: "/login",
+      templateUrl: "src/view/login.tmpl"
     })
-    .state('changepage', {
-      url: "/changepage",
-      templateUrl: "src/view/changepage.tmpl"
+    .state('enrollment', {
+      url: "/enrollment",
+      templateUrl: "src/view/enrollment.tmpl"
+    })
+    .state('profile', {
+      url: "/profile",
+      templateUrl: "src/view/profile.tmpl"
+    })
+    .state('report', {
+      url: "/report",
+      templateUrl: "src/view/report.tmpl"
+    })
+    .state('courseDes', {
+      url: "/courseDes",
+      templateUrl: "src/view/courseDes.tmpl"
     })
 });
