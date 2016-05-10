db698db2317965cc4c1d0be7b64dd40a1c8c3e2e
Nichy Han <vincentvictor@windowslive.com>
2016-05-10 12:28:31 +0700

    deploy4

--- src/js/controller/reportCtrl.js
+++ src/js/controller/reportCtrl.js
@@ -8,8 +8,8 @@ angular.module('todoApp')
 		  $http.get('http://52.37.98.127:3000/v1/5610545684?pin=5684')
         .success(function(response){
           console.log("DB " + response);
-          self.enroll_list = response.mycourse;
-          self.section_list = response.mysection;
+          self.enroll_list = response.myCourse;
+          self.section_list = response.mySection;
           self.regisType_list = response.myRegisType;
           for (val of self.enroll_list){
               self.credit_list.push(val.credit.total);
